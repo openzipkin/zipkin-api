3539b39a4a58b57c5f6020a82b898cc8891ed1af
Adrian Cole <adriancole@users.noreply.github.com>
2019-04-30 11:39:15 +0800

    Adds validation of protobuf api (#68)

--- validate.test.js
+++ validate.test.js
@@ -14,20 +14,18 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-'use strict';
-
-const Sway = require('sway');
-const read = require('fs').readFileSync;
-const load = require('js-yaml').load;
-
-function validateSwagger(yaml, validationCallback) {
-  const zipkinAPI = read(yaml).toString();
-  Sway.create({definition: load(zipkinAPI)}).then(api => {
-    validationCallback(api.validate());
-  });
-}
-
 describe('Zipkin Http Api', () => {
+  const Sway = require('sway');
+  const read = require('fs').readFileSync;
+  const load = require('js-yaml').load;
+
+  function validateSwagger(yaml, validationCallback) {
+    const zipkinAPI = read(yaml).toString();
+    Sway.create({definition: load(zipkinAPI)}).then(api => {
+      validationCallback(api.validate());
+    });
+  }
+
   it('/api/v1 yaml should have no swagger syntax errors', done => {
     validateSwagger('./zipkin-api.yaml', result => {
       expect(result.errors).toHaveLength(0);
@@ -38,6 +36,36 @@ describe('Zipkin Http Api', () => {
   it('/api/v1 yaml should have no swagger syntax errors', done => {
     validateSwagger('./zipkin2-api.yaml', result => {
       expect(result.errors).toHaveLength(0);
+      done();
+    });
+  });
+});
+
+describe('Zipkin Protocol Buffers Api', () => {
+  const load = require('protobufjs').load;
+
+  function validateProto(proto, validationCallback) {
+    load(proto, (err, root) => {
+      if (err) throw err;
+      validationCallback(root);
+    });
+  }
+
+  it('should include core data structures', done => {
+    validateProto('zipkin.proto', root => {
+      expect(root.lookupType("zipkin.proto3.Endpoint")).toBeDefined();
+      expect(root.lookupType("zipkin.proto3.Annotation")).toBeDefined();
+      expect(root.lookupType("zipkin.proto3.Span")).toBeDefined();
+      expect(root.lookupType("zipkin.proto3.ListOfSpans")).toBeDefined();
+      done();
+    });
+  });
+
+  it('should include reporting service', done => {
+    validateProto('zipkin.proto', root => {
+      // lookup is different for services vs messages
+      expect(root.lookup("SpanService")).toBeDefined();
+      expect(root.lookupType("zipkin.proto3.ReportResponse")).toBeDefined();
       done();
     });
   });
