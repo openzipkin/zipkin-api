1ae72c4a58a62ae5be1bcdc7185cb3a7e144e836
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2024-02-20 09:23:00 +0100

    Updated vite config

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -12,7 +12,7 @@
 /// the License.
 ///
 
-import path from 'node:path';
+import {resolve} from 'node:path';
 import { defineConfig, normalizePath } from "vite";
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 
@@ -22,15 +22,15 @@ export default defineConfig({
         viteStaticCopy({
             targets: [
                 {
-                    src: normalizePath(path.resolve(__dirname, '../zipkin-api.yaml')),
+                    src: normalizePath(resolve(__dirname, '../zipkin-api.yaml')),
                     dest: '',
                 },
                 {
-                    src: normalizePath(path.resolve(__dirname, '../zipkin.proto')),
+                    src: normalizePath(resolve(__dirname, '../zipkin.proto')),
                     dest: '',
                 },
                 {
-                    src: normalizePath(path.resolve(__dirname, '../zipkin2-api.yaml')),
+                    src: normalizePath(resolve(__dirname, '../zipkin2-api.yaml')),
                     dest: '',
                 },
             ],
