08f96d6130d29b126e710e238abbbdbee320aef0
Anuraag Agrawal <anuraaga@gmail.com>
2024-02-20 17:03:58 +0900

    Use vite-plugin-static-copy to copy files

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,5 +1,25 @@
-import {defineConfig} from "vite";
+import path from 'node:path';
+import { defineConfig, normalizePath } from "vite";
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-    base:'/zipkin-api/'
+    base:'/zipkin-api/',
+    plugins: [
+        viteStaticCopy({
+            targets: [
+                {
+                    src: normalizePath(path.resolve(__dirname, '../zipkin-api.yaml')),
+                    dest: '',
+                },
+                {
+                    src: normalizePath(path.resolve(__dirname, '../zipkin.proto')),
+                    dest: '',
+                },
+                {
+                    src: normalizePath(path.resolve(__dirname, '../zipkin2-api.yaml')),
+                    dest: '',
+                },
+            ],
+        }),
+    ],
 })
