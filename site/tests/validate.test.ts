39de04b96d0da56a889258d0eb33e2a584a6dcca
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2024-02-21 14:20:26 +0100

    Fixed cli tests.

--- site/tests/validate.test.ts
+++ site/tests/validate.test.ts
@@ -1,3 +1,17 @@
+///
+/// Copyright 2018-2024 The OpenZipkin Authors
+///
+/// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+/// in compliance with the License. You may obtain a copy of the License at
+///
+/// http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software distributed under the License
+/// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+/// or implied. See the License for the specific language governing permissions and limitations under
+/// the License.
+///
+
 /*
  * Copyright 2018-2019 The OpenZipkin Authors
  *
@@ -11,59 +25,58 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
-describe('Zipkin Http Api', () => {
-  const Sway = require('sway');
-  const read = require('fs').readFileSync;
-  const load = require('js-yaml').load;
 
-  function validateSwagger(yaml, validationCallback) {
+import {describe, it, expect} from "vitest";
+import {load} from 'protobufjs'
+import {create} from 'sway'
+import {readFileSync as read} from 'fs'
+import {load as loady} from 'js-yaml'
+
+describe('Zipkin Http Api', () => {
+
+  function validateSwagger(yaml: string, validationCallback:Function) {
     const zipkinAPI = read(yaml).toString();
-    Sway.create({definition: load(zipkinAPI)}).then(api => {
+    create({definition: loady(zipkinAPI) as string}).then(api => {
       validationCallback(api.validate());
     });
   }
 
-  it('/api/v1 yaml should have no swagger syntax errors', done => {
-    validateSwagger('./zipkin-api.yaml', result => {
+  it('/api/v1 yaml should have no swagger syntax errors', () => {
+    validateSwagger('../zipkin-api.yaml', result => {
       expect(result.errors).toHaveLength(0);
-      done();
     });
   });
 
-  it('/api/v2 yaml should have no swagger syntax errors', done => {
-    validateSwagger('./zipkin2-api.yaml', result => {
+  it('/api/v2 yaml should have no swagger syntax errors', () => {
+    validateSwagger('../zipkin2-api.yaml', result => {
       expect(result.errors).toHaveLength(0);
-      done();
     });
   });
 });
 
 describe('Zipkin Protocol Buffers Api', () => {
-  const load = require('protobufjs').load;
 
-  function validateProto(proto, validationCallback) {
-    load(proto, (err, root) => {
+  function validateProto(proto: any, validationCallback: Function) {
+    load(proto, (err:any, root) => {
       if (err) throw err;
       validationCallback(root);
     });
   }
 
   it('should include core data structures', done => {
-    validateProto('zipkin.proto', root => {
+    validateProto('../zipkin.proto', root => {
       expect(root.lookupType("zipkin.proto3.Endpoint")).toBeDefined();
       expect(root.lookupType("zipkin.proto3.Annotation")).toBeDefined();
       expect(root.lookupType("zipkin.proto3.Span")).toBeDefined();
       expect(root.lookupType("zipkin.proto3.ListOfSpans")).toBeDefined();
-      done();
     });
   });
 
-  it('should include reporting service', done => {
-    validateProto('zipkin.proto', root => {
+  it('should include reporting service', () => {
+    validateProto('../zipkin.proto', root => {
       // lookup is different for services vs messages
       expect(root.lookup("SpanService")).toBeDefined();
       expect(root.lookupType("zipkin.proto3.ReportResponse")).toBeDefined();
-      done();
     });
   });
 });
