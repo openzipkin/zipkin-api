115da0b8efd35745047b0729fd5d41672df04e86
Adrian Cole <64215+codefromthecrypt@users.noreply.github.com>
2024-03-04 06:33:29 +0800

    Removes the TopBar form (#103)
    This removes the TopBar form, which removes any problems around it.
    Our doc page is only for our current API.
    
    Note, this doesn't need any custom build to accomplish, just a
    (non-intuitive) change to swagger-initializer.js.
    
    As this change is against the gh-pages branch, I can't also update
    build-bin/README.md on master. I'll do that in a separate PR once it is
    verified to fix the issue.
    
    Later, we can make GH automation to regenerate the dist by unzipping the
    swagger dist overwriting everything except our IDL and
    swagger-initializer.js.
    
    Signed-off-by: Adrian Cole <adrian@tetrate.io>

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -1,18 +1,21 @@
-window.onload = function() {
+window.onload = function () {
   //<editor-fold desc="Changeable Configuration Block">
 
-  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  // Changes from defaults include the url and skipping the first standalone
+  // preset, as that loads the TopBar. We don't need to load arbitrary API
+  // definitions and forms increase the XSS attack surface.
   window.ui = SwaggerUIBundle({
     url: "https://zipkin.io/zipkin-api/zipkin2-api.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
-      SwaggerUIBundle.presets.apis,
-      SwaggerUIStandalonePreset
+        SwaggerUIBundle.presets.apis,
+
+      // Retain defaults, except index zero, which is the TopBar.
+      // https://github.com/swagger-api/swagger-ui/blob/v5.11.8/src/standalone/presets/standalone/index.js#L10
+      SwaggerUIStandalonePreset.slice(1)
     ],
-    plugins: [
-      SwaggerUIBundle.plugins.DownloadUrl
-    ],
+    plugins: [SwaggerUIBundle.plugins.DownloadUrl],
     layout: "StandaloneLayout"
   });
 
